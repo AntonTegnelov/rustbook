16a01e045c35718c027a776438d1cc9cac7be4a2
AntonT <anton.tegnelov@gmail.com>
2023-11-11 16:33:35 +0100

    adds guessing

---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/antont/Documents/programming/rust/rustbook/projects/guessing_game/target/debug/guessing_game: /home/antont/Documents/programming/rust/rustbook/projects/guessing_game/src/main.rs
